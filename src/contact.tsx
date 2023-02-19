964b176af93b2f13c0600f639d10cf1dce92c58c
John Le <john.le192@gmail.com>
2023-02-18 23:56:01 -0600

    extract icon link component

--- src/contact.tsx
+++ src/contact.tsx
@@ -1,42 +1,15 @@
 import { Component } from "react"
+import IconLink from "./iconLink"
 
 class Contact extends Component {
   render() {
     return <div className="contact">
       <p> Connect With Me </p>
       <div className="contact-icons">
-        <a
-          className="icon-link"
-          href="https://github.com/johnle192"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="icon fa-brands fa-github fa-lg" />
-        </a>
-        <a
-          className="icon-link"
-          href="https://www.instagram.com/johnle192/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="icon fa-brands fa-instagram fa-lg" />
-        </a>
-        <a
-          className="icon-link"
-          href="https://www.linkedin.com/in/john-le-3bb9bb93/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="icon fa-brands fa-linkedin fa-lg" />
-        </a>
-        <a
-          className="icon-link"
-          href="mailto:john.le192@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="icon fa-solid fa-envelope fa-lg" />
-        </a>
+        <IconLink link="https://github.com/johnle192" iconName="fa-brands fa-github" />
+        <IconLink link="https://www.instagram.com/johnle192/" iconName="fa-brands fa-instagram" />
+        <IconLink link="https://www.linkedin.com/in/john-le-3bb9bb93/" iconName="fa-brands fa-linkedin" />
+        <IconLink link="mailto:john.le192@gmail.com" iconName="fa-solid fa-envelope" />
       </div>
     </div>
   };
