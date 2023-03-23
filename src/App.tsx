efc84a3ea560b4917c917568f21e4a3a9f60dc3b
John Le <john.le192@gmail.com>
2023-03-22 23:27:30 -0500

    add basic react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import 'App.css';
 import About from 'components/About';
 import Contact from 'components/Contact';
 import Resume from 'components/Resume';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <main className="App-main">
-        <About />
-        <Resume />
-        <Contact />
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <main className="App-main">
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/resume" element={<Resume />} />
+          </Routes>
+          <Contact />
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
