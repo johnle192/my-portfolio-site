5e6f11d5844d196f92b22e28cab60a56abad0a3f
John Le <john.le192@gmail.com>
2023-09-04 14:31:21 -0700

    hover and nav active

--- src/components/IconLink.tsx
+++ src/components/IconLink.tsx
@@ -6,7 +6,7 @@ export interface IconLinkProps {
 export default function IconLink({ link, iconName }: IconLinkProps) {
   return (
     <a
-      className="icon-link m-2"
+      className="icon-link m-2 hover:text-white"
       href={link}
       target="_blank"
       rel="noopener noreferrer"
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import Contact from './Contact';
 
 const Nav: FC = () => {
+  const [activeNavItem, setActiveNavItem] = useState<string>('about');
+
   return (
     <div className="nav-container p-12 shadow-xl primary-container">
       <h1 className="nav-header headline-large">
@@ -14,14 +16,27 @@ const Nav: FC = () => {
         </NavLink>
       </h1>
       <ul className="nav-links label-large">
+        {/*TODO: figure out what else you want to do when active*/}
         <li>
-          <NavLink to="/">
-            <h3 className="headline-small">About</h3>
+          <NavLink
+            to="/"
+            className={`headline-small hover:text-white ${
+              activeNavItem === 'about' ? 'text-white' : ''
+            }`}
+            onClick={() => setActiveNavItem('about')}
+          >
+            About
           </NavLink>
         </li>
         <li>
-          <NavLink to="/resume">
-            <h3 className="headline-small">Resume</h3>
+          <NavLink
+            to="/resume"
+            className={`headline-small hover:text-white ${
+              activeNavItem === 'resume' ? 'text-white' : ''
+            }`}
+            onClick={() => setActiveNavItem('resume')}
+          >
+            Resume
           </NavLink>
         </li>
       </ul>
